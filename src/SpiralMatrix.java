1fa5c0b3e1f16c20cdf99e9c69f491902457f6dc
Saranya-Ramadasan <rsnyaramadasan04@gmail.com>
2026-04-08 16:08:02 +0530

    first commit

--- src/SpiralMatrix.java
+++ src/SpiralMatrix.java
@@ -1,8 +1,55 @@
+import java.util.Scanner;
+
 public class SpiralMatrix {
     
-        // TODO: Read r and c
-        // TODO: Read the matrix elements
-        // TODO: Print the elements in spiral order
-        // Hint: Use 4 pointers: top, bottom, left, right
+        public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        if (!sc.hasNextInt()) return;
+        
+        int r = sc.nextInt();
+        int c = sc.nextInt();
+        int[][] matrix = new int[r][c];
+        
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+        
+        int top = 0, bottom = r - 1;
+        int left = 0, right = c - 1;
+        
+        StringBuilder result = new StringBuilder();
+        
+        while (top <= bottom && left <= right) {
+            for (int i = left; i <= right; i++) {
+                result.append(matrix[top][i]).append(" ");
+            }
+            top++;
+            
+            for (int i = top; i <= bottom; i++) {
+                result.append(matrix[i][right]).append(" ");
+            }
+            right--;
+            
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    result.append(matrix[bottom][i]).append(" ");
+                }
+                bottom--;
+            }
+            
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    result.append(matrix[i][left]).append(" ");
+                }
+                left++;
+            }
+        }
+        
+        System.out.println(result.toString().trim());
+        sc.close();
+    }
     
 }
